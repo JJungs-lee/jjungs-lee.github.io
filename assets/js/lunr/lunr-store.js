3e50e9b439f13b378385bb7e727ba7f935259e40
JJungs-lee <loveljhs2@gmail.com>
2020-02-13 15:14:58 +0000

    Updating to jjungs-lee/jjungs-page@2f86f813b2ccd0194cbea5274af9614f9f172bac.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,1 @@
-var store = [{
-        "title": "Swimming ",
-        "excerpt":"      Your browser does not support the video tag.  ","categories": [],
-        "tags": [],
-        "url": "https://jjungs-lee.github.io//swim/test/",
-        "teaser":null}]
+var store = []
